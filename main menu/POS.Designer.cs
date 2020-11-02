d69f76fd24683862d0352eadfe34944403f61faa
Arvinder Singh <asing111@nyit.edu>
2020-11-02 13:19:13 -0500

    Updated POS

--- main menu/POS.Designer.cs
+++ main menu/POS.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.lblOrders = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -53,87 +55,6 @@
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Candara", 14F);
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
-            this.button1.Location = new System.Drawing.Point(6, 393);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 54);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Checkout";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Candara", 14F);
-            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
-            this.button2.Location = new System.Drawing.Point(6, 248);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(193, 54);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Add Item";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(190)))), ((int)(((byte)(198)))));
-            this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(59, 71);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(206, 647);
-            this.groupBox1.TabIndex = 11;
-            this.groupBox1.TabStop = false;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(190)))), ((int)(((byte)(198)))));
-            this.richTextBox1.Location = new System.Drawing.Point(331, 71);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(814, 647);
-            this.richTextBox1.TabIndex = 12;
-            this.richTextBox1.Text = "";
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Candara", 14F);
-            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
-            this.button3.Location = new System.Drawing.Point(6, 323);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(193, 54);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "Remove Item";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Candara", 14F);
-            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
-            this.button4.Location = new System.Drawing.Point(7, 92);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(193, 54);
-            this.button4.TabIndex = 14;
-            this.button4.Text = "View Stock";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Candara", 14F);
-            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
-            this.button5.Location = new System.Drawing.Point(6, 587);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(193, 54);
-            this.button5.TabIndex = 14;
-            this.button5.Text = "Exit";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // lblOrders
             // 
             this.lblOrders.AutoSize = true;
@@ -147,6 +68,111 @@
             this.lblOrders.Text = "Cart";
             this.lblOrders.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblOrders.Click += new System.EventHandler(this.lblOrders_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(190)))), ((int)(((byte)(198)))));
+            this.richTextBox1.Location = new System.Drawing.Point(331, 71);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(814, 647);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(190)))), ((int)(((byte)(198)))));
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(59, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 647);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Candara", 14F);
+            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button7.Location = new System.Drawing.Point(6, 527);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(193, 54);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Returns";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Candara", 14F);
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button6.Location = new System.Drawing.Point(7, 311);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(193, 54);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Apply Discount";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Candara", 14F);
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button5.Location = new System.Drawing.Point(6, 587);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(193, 54);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Candara", 14F);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button4.Location = new System.Drawing.Point(6, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 54);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "View Stock";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Candara", 14F);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button3.Location = new System.Drawing.Point(6, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 54);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Remove Item";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Candara", 14F);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button2.Location = new System.Drawing.Point(6, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 54);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Add Item";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Candara", 14F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(17)))), ((int)(((byte)(8)))));
+            this.button1.Location = new System.Drawing.Point(6, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 54);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Checkout";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // POS
             // 
@@ -175,5 +201,7 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label lblOrders;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
     }
 }
